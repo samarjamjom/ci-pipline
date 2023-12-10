b237439ea43b80ba51d0c3da62a65951254776d6
SamarJamjom <samarjamjom@gmail.com>
2023-12-10 14:06:53 +0300

    ci test2

--- test_mycode.py
+++ test_mycode.py
@@ -6,7 +6,7 @@ def test_add_positive():
     assert add_numbers(1,2)==3
 
 def test_add_zero():
-    assert add_numbers(1,0)==1
+    assert add_numbers(1,1)==1
 
 def test_add_negative():
     assert add_numbers(5,-100)==-95
